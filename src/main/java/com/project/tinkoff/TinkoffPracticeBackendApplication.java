44ae38337e17c625dd80776798908cdd393954f1
Arseny <20mineslend14@gmail.com>
2023-11-17 19:57:17 +0500

    init

---
+++ src/main/java/com/project/tinkoff/TinkoffPracticeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.tinkoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffPracticeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkoffPracticeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tinkoff/TinkoffPracticeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tinkoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffPracticeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

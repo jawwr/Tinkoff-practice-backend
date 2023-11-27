2046a8715aef6025ec4da7dda8dbc56b917c5a79
Arseny <20mineslend14@gmail.com>
2023-11-27 16:36:24 +0500

    refactor: refactor dependency and create swagger for card controller

--- src/main/java/com/project/tinkoff/rest/v1/api/CardsApi.java
+++ src/main/java/com/project/tinkoff/rest/v1/api/CardsApi.java
@@ -2,29 +2,37 @@ package com.project.tinkoff.rest.v1.api;
 
 import com.project.tinkoff.rest.v1.models.request.CardRequest;
 import com.project.tinkoff.rest.v1.models.response.CardResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequestMapping("/api/v1/projects/{projectId}/cards")
+@Tag(name = "Cards")
 public interface CardsApi {
+    @Operation(summary = "Get all cards")
     @GetMapping("/")
     ResponseEntity<List<CardResponse>> getAllCards(@PathVariable("projectId") long projectId);
 
+    @Operation(summary = "Get card by id")
     @GetMapping("/{cardId}")
     ResponseEntity<CardResponse> getCardById(@PathVariable("projectId") long projectId,
                                              @PathVariable("cardId") long cardId);
 
+    @Operation(summary = "Create card")
     @PostMapping("/")
     ResponseEntity<CardResponse> createCard(@PathVariable("projectId") long projectId,
                                             @RequestBody CardRequest card);
 
+    @Operation(summary = "Update card")
     @PutMapping("/{cardId}")
     ResponseEntity<CardResponse> updateCard(@PathVariable("projectId") long projectId,
                                             @PathVariable("cardId") long cardId,
                                             @RequestBody CardRequest card);
 
+    @Operation(summary = "Delete card")
     @DeleteMapping("/{cardId}")
     ResponseEntity<Boolean> deleteCard(@PathVariable("projectId") long projectId,
                                        @PathVariable("cardId") long cardId);

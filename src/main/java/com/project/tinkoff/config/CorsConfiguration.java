82ac70ccd56eb86ad205d0c14d7884c2a6fa2c73
Arseny <20mineslend14@gmail.com>
2023-12-02 17:30:24 +0500

    add cors config

---
+++ src/main/java/com/project/tinkoff/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.project.tinkoff.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowCredentials(false);
+    }
+}

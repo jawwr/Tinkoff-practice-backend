51ed5ff7e70545a72bca4a6d966fea13625d0e1c
Arseny <20mineslend14@gmail.com>
2023-12-06 19:04:59 +0500

    fix security config

--- src/main/java/com/project/tinkoff/config/AuthConfig.java
+++ src/main/java/com/project/tinkoff/config/AuthConfig.java
@@ -28,10 +28,12 @@ public class AuthConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("*/v1/auth/**")
+                    auth.requestMatchers("/api/**")
                             .permitAll();
-                    auth.requestMatchers("*/v1/**")
-                            .authenticated();
+//                    auth.requestMatchers("*/v1/auth/**")
+//                            .permitAll();
+//                    auth.requestMatchers("*/v1/**")
+//                            .authenticated();
                     auth.requestMatchers(HttpMethod.POST)
                             .authenticated();
                     auth.requestMatchers(HttpMethod.DELETE)
